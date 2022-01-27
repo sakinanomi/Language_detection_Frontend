ef667ef3aff0c931ab55c40f58d67df2c2b82877
sakinanomi <56465213+sakinanomi@users.noreply.github.com>
2022-01-27 18:47:02 +0530

    Addign the first

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask import render_template,request,redirect
+import requests
+
+app = Flask(__name__)
+ans=None
+@app.route('/')
+def index():
+	global ans
+
+	return render_template('index.html',ans=ans)
+
+@app.route('/send',methods=['GET','POST'])
+def send():
+	if request.method == 'POST':
+		text = (request.form.get('text'))
+
+		data={'text':text}
+
+		language = requests.post('https://la-identification.herokuapp.com/predict', data=data)
+
+		final = (language.json()['predicted'])
+
+		print(final)
+
+
+
+
+	return render_template('index.html',ans=final)
+
+
+
+
+if __name__ == '__main__':
+
+	app.run(debug=True )
